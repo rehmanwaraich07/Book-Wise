e0cac34131f0c3a586752ae1ae7a72ff8e75b8a3
M.Rehman Waraich <rehmanwraich2023@gmail.com>
2025-01-28 21:16:34 +0500

    shadcn installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Book Wise</div>
+   <>
+    <Button variant={"secondary"}>Click me</Button>
+   </>
   );
 }
