316b0ec11c25f6850406a5c6ef4bbbc77e3cfe06
M.Rehman Waraich <rehmanwraich2023@gmail.com>
2025-02-05 10:38:22 +0500

    updates home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
     <>
       <h1>This is a web application Saas</h1>
+      <p>we will complete this soon.</p>
       <p>we will complete this soon.</p>
     </>
   );
